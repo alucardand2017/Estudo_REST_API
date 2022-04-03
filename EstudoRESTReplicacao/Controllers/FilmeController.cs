358ffdc9f0ee6e6d7952646323d02db5fdd88212
Anderson Silva <anderson_ns5@hotmail.com>
2022-04-02 23:06:39 -0300

    Add project files.

---
+++ EstudoRESTReplicacao/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using EstudoRESTReplicacao.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EstudoRESTReplicacao.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id}, filme);
+        }
+        
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filmes => filmes.Id == id);
+            if (filme == null)
+                return NotFound();
+            return Ok(filme);
+        }
+    }
+}
---
+++ EstudoRESTReplicacao/Models/Filme.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstudoRESTReplicacao.Models
+{
+    public class Filme
+    {
+        private const int duracaoMinima1 = 1;
+        private const int duracaoMaxima1 = 600;
+
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Titulo do filme é obrigatorio!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Preencha o campo Genero.")]
+        public string Genero { get; set; }
+        [Required(ErrorMessage ="Preencha o campo Diretor")]
+        public string Diretor { get; set; }
+        [Range(duracaoMinima1, duracaoMaxima1, ErrorMessage = "Escolha valores entre e 1 e 600")]
+        public int Duracao  { get; set; }
+        public override string ToString()
+        {
+            return Titulo + " / " + Genero + " / " + Diretor + " / " + Duracao.ToString();   
+        }
+    }
+
+    
+
+}
